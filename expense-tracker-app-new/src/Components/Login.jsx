c5e5a9f59bb9b20ec0d675ef3901337b238f4821
riya-malik31 <riyacws123@gmail.com>
2025-04-08 12:10:52 +0530

    Update Login.jsx

--- expense-tracker-app-new/src/Components/Login.jsx
+++ expense-tracker-app-new/src/Components/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://expense-tracker-app-backend-steel.vercel.app/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(inputs),
